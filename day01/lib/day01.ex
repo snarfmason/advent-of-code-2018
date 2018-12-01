ca734987f41344870edf0279c045501631b7baf4
Jon Mason <jon@stembolt.com>
2018-12-01 14:46:56 -0800

    Day 1, part 2

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -25,15 +25,50 @@ defmodule Day01 do
   +1, +1, -2 results in  0
   -1, -2, -3 results in -6
     Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+
+  --- Part Two ---
+  You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+  For example, using the same list of changes above, the device would loop as follows:
+
+  Current frequency  0, change of +1; resulting frequency  1.
+  Current frequency  1, change of -2; resulting frequency -1.
+  Current frequency -1, change of +3; resulting frequency  2.
+  Current frequency  2, change of +1; resulting frequency  3.
+  (At this point, the device continues from the start of the list.)
+  Current frequency  3, change of +1; resulting frequency  4.
+  Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+  In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+
+  Here are other examples:
+
+  +1, -1 first reaches 0 twice.
+  +3, +3, +4, -2, -4 first reaches 10 twice.
+  -6, +3, +8, +5, -6 first reaches 5 twice.
+  +7, +7, -2, -7, -4 first reaches 14 twice.
+  What is the first frequency your device reaches twice?
   """
 
   @doc """
   run part1:
   Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
   """
-  def part1 do
-    list = String.split(File.read!("input.txt"))
-    compute_frequency(0, Enum.map(list, &String.to_integer/1))
+  def part1, do: compute_frequency(0, input())
+
+  @doc """
+  run part2:
+  What is the first frequency your device reaches twice?
+  """
+  def part2, do: duplicate_frequncy(input())
+
+  @doc """
+  input.txt contents mapped to one integer per line
+  """
+  def input do
+    "input.txt"
+    |> File.read!()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
   end
 
   @doc """
@@ -53,5 +88,44 @@ defmodule Day01 do
 
   def compute_frequency(initial, [next | rest]) do
     compute_frequency(initial + next, rest)
+  end
+
+  @doc """
+  computes a frequncy from an initial and a list of changes.
+
+  ## Examples
+
+      iex> Day01.duplicate_frequncy([1, -1])
+      0
+      iex> Day01.duplicate_frequncy([1, -2, 3, 1])
+      2
+  """
+  def duplicate_frequncy(list), do: duplicate_frequncy(0, list, list, MapSet.new())
+
+  def duplicate_frequncy(current_f, [], original_list, seen) do
+    duplicate_frequncy(current_f, original_list, original_list, seen)
+  end
+
+  def duplicate_frequncy(current_f, list, original_list, seen) do
+    duplicate_frequncy(
+      current_f,
+      list,
+      original_list,
+      seen,
+      MapSet.member?(seen, current_f)
+    )
+  end
+
+  def duplicate_frequncy(current_f, _, _, _, true) do
+    current_f
+  end
+
+  def duplicate_frequncy(current_f, [next_f | rest], original_list, seen, false) do
+    duplicate_frequncy(
+      current_f + next_f,
+      rest,
+      original_list,
+      MapSet.put(seen, current_f)
+    )
   end
 end
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -2,23 +2,39 @@ defmodule Day01Test do
   use ExUnit.Case
   doctest Day01
 
-  test "example 1" do
+  test "compute_frequency example 1" do
     list = [1, -2, 3, 1]
     assert Day01.compute_frequency(0, list) == 3
   end
 
-  test "example 2" do
+  test "compute_frequency example 2" do
     list = [1, 1, 1]
     assert Day01.compute_frequency(0, list) == 3
   end
 
-  test "example 3" do
+  test "compute_frequency example 3" do
     list = [1, 1, -2]
     assert Day01.compute_frequency(0, list) == 0
   end
 
-  test "example 4" do
+  test "compute_frequency example 4" do
     list = [-1, -2, -3]
     assert Day01.compute_frequency(0, list) == -6
   end
+
+  test "duplicate_frequncy exmaple 2" do
+    list = [3, 3, 4, -2, -4]
+    assert Day01.duplicate_frequncy(list) == 10 
+  end
+
+  test "duplicate_frequncy exmaple 3" do
+    list = [-6, 3, 8, 5, -6]
+    assert Day01.duplicate_frequncy(list) == 5 
+  end
+
+  test "duplicate_frequncy exmaple 4" do
+    list = [7, 7, -2, -7, -4]
+    assert Day01.duplicate_frequncy(list) == 14 
+  end
+
 end
